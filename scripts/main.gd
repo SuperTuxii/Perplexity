e14e46fe0a6f7998fd76a0f1690299e6bef1a169
SuperTuxii <123881249+SuperTuxii@users.noreply.github.com>
2025-09-28 16:31:42 +0200

    Added the ability to focus the monitor, when clicking while hovering over it

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,6 +2,8 @@ class_name Main extends Node3D
 
 @export
 var outline_material: Material = preload("res://materials/OutlineMaterial.tres")
+@onready
+var camera: Camera3D = $"Desk Setup/Chair/Camera"
 
 var paused: bool:
 	get:
@@ -14,16 +16,37 @@ var drag_sensitivity: float:
 	set(value):
 		$PauseMenu.drag_sensitivity = value
 
+var focus: int = 0:
+	set = set_focus
+
 var mouse_motion: Vector2 = Vector2()
 var mouse_pressed: bool = false
 var mouse_movement: float = 0
 
 func _process(_delta: float) -> void:
-	mouse_motion.y = clamp(mouse_motion.y, -1.56, 1.56)
-	$"Desk Setup/Chair/Camera".transform.basis = Basis.from_euler(Vector3(mouse_motion.y, 0, 0))
-	$"Desk Setup/Chair".transform.basis = Basis.from_euler(Vector3(0, mouse_motion.x, 0))
+	if focus == 0:
+		mouse_motion.y = clamp(mouse_motion.y, -1.56, 1.56)
+		camera.transform.basis = Basis.from_euler(Vector3(mouse_motion.y, 0, 0))
+		$"Desk Setup/Chair".transform.basis = Basis.from_euler(Vector3(0, mouse_motion.x, 0))
 	if Input.is_action_just_pressed("pause"):
-		paused = !paused
+		if focus == 0:
+			paused = !paused
+		else:
+			focus = 0
+
+func set_focus(index: int) -> void:
+	focus = index
+	match index:
+		0:
+			mouse_motion = Vector2()
+			camera.reparent($"Desk Setup/Chair")
+			camera.position = Vector3(0, 1.2, 0)
+			camera.rotation = Vector3()
+		1:
+			$"Desk Setup/Monitor/Monitor".material_overlay = null
+			camera.reparent($"Desk Setup/Monitor", false)
+			camera.position = Vector3(0.3, 0.34, 0)
+			camera.rotation_degrees = Vector3(0, 90, 0)
 
 func _input(event: InputEvent) -> void:
 	if (!paused):
@@ -39,11 +62,11 @@ func _input(event: InputEvent) -> void:
 func _on_monitor_area_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if (event is InputEventMouseButton and event.button_index == MouseButton.MOUSE_BUTTON_LEFT) or event is InputEventScreenTouch:
 		if !event.pressed and mouse_movement < 0.05:
-			pass
-			#print("Interacted with Monitor!")
+			focus = 1
 
 func _on_monitor_area_mouse_entered() -> void:
-	$"Desk Setup/Monitor/Monitor".material_overlay = outline_material
+	if focus != 1:
+		$"Desk Setup/Monitor/Monitor".material_overlay = outline_material
 
 func _on_monitor_area_mouse_exited() -> void:
 	$"Desk Setup/Monitor/Monitor".material_overlay = null
