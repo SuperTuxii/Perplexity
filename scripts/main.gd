92ed3c57f32c69a83f644e7f7f18238d32564c66
SuperTuxii <123881249+SuperTuxii@users.noreply.github.com>
2025-09-29 11:56:39 +0200

    Making animations for the start of the game

--- scripts/main.gd
+++ scripts/main.gd
@@ -26,9 +26,17 @@ var focus_to_rotation: Vector3 = Vector3()
 var focus: int = 0:
 	set = set_focus
 
+var head_fall_weight: float = -1
+
 var mouse_motion: Vector2 = Vector2()
 var mouse_pressed: bool = false
 var mouse_movement: float = 0
+
+func _ready() -> void:
+	$ScreenOverlays.set_subtitles("I'm feeling a bit tire…", 2.5)
+	$ScreenOverlays.eyes_move_weight = 0
+	head_fall_weight = 0
+	focus = -1
 
 func _process(delta: float) -> void:
 	if focus == 0 and focus_weight == -1: # Applying screen drag when focus is 0
@@ -50,17 +58,27 @@ func _process(delta: float) -> void:
 			camera.rotation = focus_to_rotation * (PI / 180)
 			focus_weight = -1
 			finished_focus()
+	if head_fall_weight != -1: # Head fall animation
+		head_fall_weight += delta * 0.25
+		head_fall_weight = minf(head_fall_weight, 1)
+		camera.rotation = camera.rotation.lerp(Vector3(deg_to_rad(-60), 0, 0), head_fall_weight ** 4)
+		if head_fall_weight == 1:
+			camera.rotation = Vector3(deg_to_rad(-60), 0, 0)
+			head_fall_weight = -1
+			$Timer.start(1)
 
 func set_focus(index: int) -> void:
 	focus = index
+	if index == -1:
+		return # Other focus like a cutscene
 	# Focus animation configuration
 	match index:
-		0:
+		0: # No focus/on chair
 			mouse_motion.y = 0
 			camera.reparent($"Desk Setup/Chair")
 			focus_to_position = Vector3(0, 1.2, 0)
 			focus_to_rotation = Vector3()
-		1:
+		1: # Monitor focused
 			$"Desk Setup/Monitor/Monitor".material_overlay = null
 			camera.reparent($"Desk Setup/Monitor")
 			focus_to_position = Vector3(0.3, 0.34, 0)
@@ -92,7 +110,13 @@ func _on_monitor_area_input_event(_camera: Node, event: InputEvent, _event_posit
 		if !event.pressed and mouse_movement < 0.05:
 			focus = 1
 func _on_monitor_area_mouse_entered() -> void:
-	if focus != 1 and focus_weight == -1:
+	if focus == 0 and focus_weight == -1:
 		$"Desk Setup/Monitor/Monitor".material_overlay = outline_material
 func _on_monitor_area_mouse_exited() -> void:
 	$"Desk Setup/Monitor/Monitor".material_overlay = null
+
+func _on_timer_timeout() -> void:
+	if focus == -1 and $ScreenOverlays.eyes_position == 0:
+		$ScreenOverlays.eyes_move_from = 0
+		$ScreenOverlays.eyes_move_to = 2
+		$ScreenOverlays.eyes_move_weight = 0
--- scripts/screen_overlays.gd
+++ scripts/screen_overlays.gd
@@ -2,15 +2,48 @@ class_name ScreenOverlays extends Control
 
 @export
 var eyes_move_speed: float = 0.5
-var eyes_move_time: float = -1
+var eyes_move_weight: float = -1
 var eyes_move_from: float = 2
 var eyes_move_to: float = 0
+var eyes_position: float:
+	get:
+		return $Eyelids.texture.gradient.get_offset(1)
+
+@export
+var subtitles_speed: float = 1
+var subtitles_show: bool = false
+var subtitles_weight: float:
+	get:
+		return $SubtitleContainer/Subtitles.visible_ratio
+	set(value):
+		$SubtitleContainer/Subtitles.visible_ratio = value
 
 func _process(delta: float) -> void:
-	if eyes_move_time != -1:
-		eyes_move_time += delta * eyes_move_speed
-		eyes_move_time = minf(eyes_move_time, 1)
-		$Eyelids.texture.gradient.set_offset(0, lerpf(eyes_move_from, eyes_move_to, eyes_move_time ** 3)-1)
-		$Eyelids.texture.gradient.set_offset(1, lerpf(eyes_move_from, eyes_move_to, eyes_move_time ** 3))
-		if eyes_move_time == 1:
-			eyes_move_time = -1
+	if eyes_move_weight != -1:
+		eyes_move_weight += delta * eyes_move_speed
+		eyes_move_weight = minf(eyes_move_weight, 1)
+		$Eyelids.texture.gradient.set_offset(0, lerpf(eyes_move_from, eyes_move_to, eyes_move_weight ** 3)-1)
+		$Eyelids.texture.gradient.set_offset(1, lerpf(eyes_move_from, eyes_move_to, eyes_move_weight ** 3))
+		if eyes_move_weight == 1:
+			eyes_move_weight = -1
+	if subtitles_weight != float(subtitles_show):
+		subtitles_weight += delta * subtitles_speed * (1 if subtitles_show else -1)
+		subtitles_weight = clampf(subtitles_weight, 0, 1)
+		if subtitles_weight == 1 and $SubtitleContainer/StayTimer.wait_time != 0:
+			$SubtitleContainer/StayTimer.start()
+		if subtitles_weight == 0:
+			$SubtitleContainer/Subtitles.text = ""
+
+func set_subtitles(subtitle: String, stay_seconds: float = 0, subtitle_speed: float = 1):
+	subtitles_show = true
+	subtitles_speed = subtitle_speed
+	$SubtitleContainer/Subtitles.text = subtitle
+	$SubtitleContainer/StayTimer.wait_time = stay_seconds
+
+func remove_subtitles():
+	subtitles_show = false
+	if !$SubtitleContainer/StayTimer.is_stopped():
+		$SubtitleContainer/StayTimer.stop()
+
+func _on_stay_timer_timeout() -> void:
+	remove_subtitles()
